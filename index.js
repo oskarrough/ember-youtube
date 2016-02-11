9c9ad19e5db8861fe36be15d23d953fd01f8fdc2
Denis Demchenko <lancedikson@gmail.com>
2016-02-11 23:23:36 +0300

    remove import moment.js to bower.json

--- index.js
+++ index.js
@@ -6,8 +6,5 @@ module.exports = {
 
   included: function(app) {
     this._super.included(app);
-
-    app.import(app.bowerDirectory + '/moment/moment.js');
-    app.import(app.bowerDirectory + '/moment-duration-format/lib/moment-duration-format.js');
   }
 };
